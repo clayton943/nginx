bcc53d6b35b44a0c76ec07a356ce5aaa6e097538
clayton943 <32561687+clayton943@users.noreply.github.com>
2017-10-05 22:19:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root..." 
+    exit 1
+fi
+
 apt -y dist-upgrade --auto-remove --purge
 apt -y install nginx
 wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/clayton943/nginx/master/nginx.conf
