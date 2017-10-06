b689ef4e231e30a006f1a3a2ac9c694365564c03
clayton943 <32561687+clayton943@users.noreply.github.com>
2017-10-05 22:15:31 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt -y dist-upgrade --auto-remove --purge
 apt -y install nginx
-wget -P /etc/nginx/nginx.conf https://raw.githubusercontent.com/clayton943/nginx/master/nginx.conf
-wget -P /etc/nginx/conf.d/proxy.conf https://raw.githubusercontent.com/clayton943/nginx/master/proxy.conf
+wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/clayton943/nginx/master/nginx.conf
+wget -O /etc/nginx/conf.d/proxy.conf https://raw.githubusercontent.com/clayton943/nginx/master/proxy.conf
 fuser -k 80/tcp
 service nginx restart
